76528af5b8b9797ddf20c713f35ba343ff0afe4e
Nikola Tasic <ntasic7@gmail.com>
2020-08-11 18:55:14 +0200

    globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,2 +1,5 @@
+// export const websiteUrl = "http://rzp.7aske.xyz"
+// export const backendUrl = "http://rzp.7aske.xyz:8080/blog"
+
 export const websiteUrl = "http://localhost:3000"
 export const backendUrl = "http://localhost:8080/blog"
