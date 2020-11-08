1f89cdd607e62367a0ad4b16e328436f8d7d9f94
Nikola Tasic <ntasic7@gmail.com>
2020-11-08 15:20:01 +0100

    switched to https

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
 export const websiteUrl = process.env.NODE_ENV === "development" ?
-	"http://localhost:3000" : "http://rzp.7aske.xyz";
+	"http://localhost:3000" : "https://rzp.7aske.xyz";
 export const backendUrl = process.env.NODE_ENV === "development" ?
-	"http://localhost:8080/rzp-blog" : "http://7aske.xyz:8080/rzp-blog";
+	"http://localhost:8080/rzp-blog" : "https://api.rzp.7aske.xyz";
